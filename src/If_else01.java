ab8bc36ab8034be7b5873d6a76573dc76a0550fb
yunseahae <kimyun0704@naver.com>
2024-03-26 21:41:16 +0900

    ex 1

---
+++ src/If_else01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class If_else01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("a = ");
+        int a = scan.nextInt();
+        System.out.println("b = ");
+        int b = scan.nextInt();
+        System.out.println("==============");
+        if(a>b){
+            System.out.println(a);
+        } else if (a<b) {
+            System.out.println(b);
+        }else {
+            System.out.println(a+", "+b);
+        }
+    }
+}
