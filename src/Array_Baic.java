2152cadccaf5f482a446ee6a8813509809f0252b
yunseahae <kimyun0704@naver.com>
2024-03-27 21:09:42 +0900

    Array

---
+++ src/Array_Baic.java
@@ -0,0 +1,26 @@
+public class Array_Baic {
+    public static void main(String[] args) {
+
+        // 배열 선언과 동시에 초기화를 진행
+        int[] myArray = {10};
+
+        // 배열 초기화와 동시에 선언 및 값 대입
+        int[] myArray2 = {1,2,3,4,5};
+
+        // 다차원배열
+        int[][] myArray3 = {{1,2,3,4,5},{1,2,3,4,5}};
+
+        // 배열 초기화
+        myArray[0] = 1;
+        myArray[1] = 1;
+        myArray[2] = 1;
+        myArray[3] = 1;
+        myArray[4] = 1;
+        myArray[5] = 1;
+        myArray[6] = 1;
+        myArray[7] = 1;
+        myArray[8] = 1;
+        myArray[9] = 1;
+
+    }
+}
