a44277fab3715fc94f419a890950bb1d33741041
yunseahae <kimyun0704@naver.com>
2024-04-15 21:40:25 +0900

    2

--- src/Inheritance_Basic.java
+++ src/Inheritance_Basic.java
@@ -14,6 +14,8 @@ class Dog extends Animal {
 
 public class Inheritance_Basic {
     public static void main(String[] args){
+        // Animal dog = new Dog(); == 가능하지만 Dog Class의 메서드는 사용할 수 없습니다. == Dog is a Animal
+       // Dog dog = new Animal(); == X 불가능 Animal is a Dog
         Dog dog = new Dog();
         dog.setName("왈왈이");
         System.out.println(dog.name);
